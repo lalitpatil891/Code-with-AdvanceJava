acf55d036f1dbb315035462b1d26a4bcc1fa6adc
Lalit Patil <lp613408@gmail.com>
2025-04-14 23:55:09 +0530

    commit

---
+++ servlet_programs/App_servlet_sendredirect_method_yt/src/main/java/com/example_of_sendredirect_method/AddServlet.java
@@ -0,0 +1,33 @@
+package com.example_of_sendredirect_method;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/add")
+public class AddServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+		
+		res.sendRedirect(location);
+		
+		req.setAttribute("k", k);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		
+		rd.forward(req, res);
+		
+	}
+	
+}
---
+++ servlet_programs/App_servlet_sendredirect_method_yt/src/main/java/com/example_of_sendredirect_method/SqServlet.java
@@ -0,0 +1,25 @@
+package com.example_of_sendredirect_method;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet {
+	
+public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		
+//		int k = Integer.parseInt(req.getParameter("k"));
+//		
+//		k = k*k ;
+//	
+//		PrintWriter out = res.getWriter();
+//		out.println("Result is" +k);
+//		
+	
+		System.out.println("sq called");
+	}
+}
